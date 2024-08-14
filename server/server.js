b1e2d149cbee257542b3ffb8296d61276219027a
lakshaybogal <rajputlakshay553@gmail.com>
2024-08-14 10:15:42 +0530

    final push

--- server/server.js
+++ server/server.js
@@ -22,12 +22,10 @@ const corsOptions = {
 app.use(cors(corsOptions));
 // middleware responsible to parse json data in request body
 
-app.get("/api", (req, res) => {
-    return res.status(200).json({ message: "Api working on api" });
-})
+
 
 app.get("/", (req, res) => {
-    return res.status(200).json({ message: "Api working on root" });
+    return res.status(200).json({ message: "Jai Mata Di" });
 })
 app.use(express.json());
 app.use('/api/auth', authRoute);
